d8ccdb3282ba4c89f8c0ddfe928e30a6c7b100e1
yelsharawy <yelsharawy20@stuy.edu>
2020-12-06 13:19:03 -0500

    Copied template and fixed formatting

---
+++ Range.java
@@ -0,0 +1,28 @@
+import java.util.NoSuchElementException;
+
+public class Range implements IntegerSequence {
+
+    private int start, end, current;
+
+    public Range(int start,  int end) {
+
+    }
+
+    public void reset() {
+
+    }
+
+    public int length() {
+
+    }
+
+    public boolean hasNext() {
+
+    }
+
+    //@throws NoSuchElementException
+    public int next() {
+
+    }
+
+}
