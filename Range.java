4314db246c2a2947780aaa6d496486051383ec0d
yelsharawy <yelsharawy20@stuy.edu>
2020-12-06 13:20:30 -0500

    Implemented constructor

--- Range.java
+++ Range.java
@@ -5,7 +5,9 @@ public class Range implements IntegerSequence {
     private int start, end, current;
 
     public Range(int start,  int end) {
-
+        this.start = start;
+        this.end = end;
+        current = start;
     }
 
     public void reset() {
