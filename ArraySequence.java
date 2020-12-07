fd445072201f224014c0b7fb3cfcd45abe442de3
yelsharawy <yelsharawy20@stuy.edu>
2020-12-07 12:31:47 -0500

    Added (unimplemented) ArraySequence.java

---
+++ ArraySequence.java
@@ -0,0 +1,27 @@
+public class ArraySequence implements IntegerSequence {
+
+    private int index;
+    private int[] arr;
+
+    public ArraySequence(int[] arr) {
+
+    }
+
+    public void reset() {
+
+    }
+
+    public int length() {
+        return 0;
+    }
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    //@throws NoSuchElementException
+    public int next() {
+        return 0;
+    }
+
+}
